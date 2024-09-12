66b666608564b8a73f7bbcf0ebcc9769e99f4266
LanDev01 <alnjr0001@outlook.com>
2024-09-12 11:18:26 -0300

    Feat: instalando react query e conectando front e back com useQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,20 @@
 import { Dialog } from './components/ui/dialog';
 import { CreateGoal } from './components/create-goal';
 import { Summary } from './components/summary';
-import { useState, useEffect } from 'react';
 import { EmptyGoals } from './components/empty-goals';
-
-type SummaryResponse = {
-  completed: number;
-  total: number;
-  goalsPerDay: Record<
-    string,
-    {
-      id: string;
-      title: string;
-      completedAt: string;
-    }
-  >;
-};
+import { useQuery } from '@tanstack/react-query';
+import { getSummary } from './http/get-summary';
 
 export function App() {
-  const [summary, setSummary] = useState<SummaryResponse | null>(null);
-
-  useEffect(() => {
-    fetch('http://localhost:3333/summary')
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        setSummary(data.summary);
-      });
-  }, []);
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60,
+  });
 
   return (
     <Dialog>
-      {summary?.total && summary.total > 0 ? <Summary /> : <EmptyGoals />}
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
 
       <CreateGoal />
     </Dialog>
