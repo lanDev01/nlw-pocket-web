5ccb339ad3a170762beb7c5eda4aa518360497e5
LanDev01 <alnjr0001@outlook.com>
2024-09-12 09:59:07 -0300

    Feat: consumindo api com useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
 import { Dialog } from './components/ui/dialog';
 import { CreateGoal } from './components/create-goal';
-// import { EmptyGoals } from './components/empty-goals';
 import { Summary } from './components/summary';
+import { useState, useEffect } from 'react';
+import { EmptyGoals } from './components/empty-goals';
+
+type SummaryResponse = {
+  completed: number;
+  total: number;
+  goalsPerDay: Record<
+    string,
+    {
+      id: string;
+      title: string;
+      completedAt: string;
+    }
+  >;
+};
 
 export function App() {
+  const [summary, setSummary] = useState<SummaryResponse | null>(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/summary')
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        setSummary(data.summary);
+      });
+  }, []);
+
   return (
     <Dialog>
-      {/* <EmptyGoals /> */}
-      <Summary />
+      {summary?.total && summary.total > 0 ? <Summary /> : <EmptyGoals />}
+
       <CreateGoal />
     </Dialog>
   );
